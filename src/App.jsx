f87f158f37430199b4b1be82c39f0877a582649f
chelseah02 <chelseah2020@gmail.com>
2023-03-22 14:26:44 -0500

    Added ban features

--- src/App.jsx
+++ src/App.jsx
@@ -2,17 +2,9 @@ import { useState } from 'react'
 import viteLogo from '/vite.svg'
 import './App.css'
 const ACCESS_KEY = import.meta.env.VITE_APP_ACCESS_KEY;
-//import APIForm from './components/APIForm';
 
 function App() {
 
-  const [inputs, setInputs] = useState({
-    format: "jpeg",
-    no_ads: "true",
-    no_cookie_banners: "true",
-    width: "1920",
-    height: "1080",
-  });
   let query = `https://api.thedogapi.com/v1/images/search?api_key=${ACCESS_KEY}&has_breeds=${true}&width=${1820}&height=${980}`;
   console.log(query);
   const [currentDog, setCurrentDog] = useState({
@@ -21,30 +13,7 @@ function App() {
     attr2: null,
     attr3: null,
   });
- /*
-  const submitForm = () => {
-    let defaultValues = {
-      format: "jpeg",
-      no_ads: "true",
-      no_cookie_banners: "true",
-      width: "1920",
-      height: "1080",
-    };
-
-  }
-
- if (inputs.url == "" || inputs.url == " ") {
-    inputs.url = "google.com";
-  }
-  else{
-    for (const [key, value] of Object.entries(inputs)) {
-      if (value == ""){
-        inputs[key] = defaultValues[key]
-      }
-    }
-    makeQuery();
-  } */
-
+  const [bannedList, setBanned] = useState([]);
   const reset = () => {
     setCurrentDog({
       image: null,
@@ -56,7 +25,7 @@ function App() {
     
   }
   const callAPI = async (query) => {
-    
+    reset();
     const response = await fetch(query);
     const json = await response.json();
     console.log("In callAPI");
@@ -65,6 +34,26 @@ function App() {
     if (json[0].url == null){
       alert("Oops! Something went wrong with that query, let's try again!")
         }
+    // If there are banned attributes, ensure that this query does not display them
+    else if (bannedList.length != 0){
+      if ((bannedList.includes(json[0].breeds[0].name)) ||
+      (bannedList.includes(json[0].breeds[0].life_span)) || 
+      (bannedList.includes(json[0].breeds[0].weight['imperial'] + " lbs")) ||
+      (bannedList.includes(json[0].breeds[0].height['imperial'] + " inches"))) {
+        callAPI(query).catch(console.error);
+      }
+
+      else {
+        setCurrentDog({
+          image: json[0].url,
+          name: json[0].breeds[0].name,
+          lifetime: json[0].breeds[0].life_span,
+          weight: json[0].breeds[0].weight['imperial'],
+          height: json[0].breeds[0].height['imperial'],
+          });
+  
+      }
+    }
     else {
       setCurrentDog({
         image: json[0].url,
@@ -73,14 +62,8 @@ function App() {
         weight: json[0].breeds[0].weight['imperial'],
         height: json[0].breeds[0].height['imperial'],
         });
-      //reset();
+
     }
-  
-  }
-  const makeQuery = () => {
-    let wait_until = "network_idle";
-    let response_type = "json";
-    let fail_on_status = "400%2C404%2C500-511";
   
   }
 
@@ -89,15 +72,40 @@ function App() {
     callAPI(query).catch(console.error); // call query when the button is clicked
   }
 
+  // Banned attributes
+  const banAttribute = (currentDog, id) => {
+    if (currentDog.image != null) {
+      if (id == 0) {
+        setBanned([...bannedList, currentDog.name]);
+        console.log(bannedList + " is the banned list.");
+      }
+      else if (id == 1) {
+        console.log("Reading lifetime!");
+        setBanned([...bannedList, currentDog.lifetime]);
+        console.log(bannedList + " is the banned list.");
+      }
+      else if (id == 2) {
+        setBanned([...bannedList, currentDog.weight + " lbs"]);
+        console.log(bannedList + " is the banned list.");
+      } 
+      else {
+        setBanned([...bannedList, currentDog.height + " inches"]);
+        console.log(bannedList + " is the banned list.");
+      }
+      console.log(bannedList);
+    }
+  }
+
   return (
     <div className="App">
-      <h1>Veni Vici!</h1>
-      <h3> Find a new Furry Friend! </h3>
-      <button onClick={handleCall}> Discover!</button>
+      <div className='main'>
+      <h1 className='title'>Veni Vici!</h1>
+      <h3 className='title-descr'> Find a new Furry Friend! </h3>
+      <button className="btn" onClick={handleCall}> Discover!</button>
       <br></br>
-      {currentDog ? (
+      {(currentDog.image != null) ? (
       <img
-        className="dog-image"
+        className="image-container"
         src={currentDog.image}
       />
     ) : (
@@ -105,37 +113,45 @@ function App() {
       )}
  
     <br></br>
-    {currentDog ? (
-      <button className="dog-name">
-        Name: {currentDog.name}
+    {(currentDog.image != null) ? (
+      <button className="btn" onClick={(e) => banAttribute(currentDog, 0)}>
+        {currentDog.name}
         </button>
     ) : (
       <div></div>
       )}
     
-      {currentDog ? (
-      <button className="dog-lifetime">
-        Lifetime: {currentDog.lifetime}
+      {(currentDog.image != null) ? (
+      <button className="btn" onClick={(e) => banAttribute(currentDog, 1)}>
+        {currentDog.lifetime}
         
         </button>
     ) : (
       <div></div>
       )}
 
-      {currentDog ? (
-      <button className="dog-weight">
-        Weight: {currentDog.weight}
+      {(currentDog.image != null) ? (
+      <button className="btn" onClick={(e) => banAttribute(currentDog, 2)}>
+        {currentDog.weight + " lbs"}
         </button>
     ) : (
       <div></div>
       )}
-      {currentDog ? (
-      <button className="dog-height">
-        Height: {currentDog.height}
+      {(currentDog.image != null) ? (
+      <button className="btn" onClick={(e) => banAttribute(currentDog, 3)}>
+        {currentDog.height + " inches"}
         </button>
     ) : (
       <div></div>
       )}
+      </div>
+      <div className='banned-section'>
+        <br></br>
+        <br></br>
+        <br></br>
+        <h2> Banned List:</h2>
+       {bannedList.map((element) => <button className="btn">{element}</button>)}
+      </div>
     </div>
 
   )
